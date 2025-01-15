a5b920f8b5c27dd20a18a3d7e68869fa16756770
Virtual Jasper <jasper2virtual@hotmail.com>
2025-01-15 21:03:26 +0800

    refactor: improve layout of desktop header and mobile images viewer for better alignment and user experience

--- src/components/desktop-header.vue
+++ src/components/desktop-header.vue
@@ -1,27 +1,33 @@
 <template>
-    <header class="fixed inset-0 bg-white mx-auto h-fit max-w-[--grid-width] flex z-10
+    <header class="fixed inset-0 bg-white h-fit  z-10 max-w-[--grid-width] mx-auto
        pt-[--desktop-header-padding-top]
         pb-[--desktop-header-padding-bottom]
         pl-[--desktop-header-padding-left]
         pr-[--desktop-header-padding-right]
         border-b">
-        <div class="flex items-center gap-12">
-            <header-logo />
-            <nav>
-                <ul class="flex gap-8 h-full items-center">
-                    <li v-for="item in props.menuItems" :key="item">
-                        <div>
-                            <a href="#" class="text-[#69707D] hover:text-black peer">{{ item }}</a>
-                            <hr class="peer-hover:shadow-[0_2px_#FF7E1B] border-none peer-hover:border-solid translate-y-7 ">
-                        </div>
+        <div class="h-[--desktop-header-height] flex items-center">
 
-                    </li>
-                </ul>
-            </nav>
-        </div>
-        <div class="ml-auto flex gap-8 items-center">
-            <ShoppingCart />
-            <Avatar />
+            <div class="flex items-center gap-12">
+                <header-logo />
+                <nav>
+                    <ul class="flex gap-8 h-full items-center">
+                        <li v-for="item in props.menuItems" :key="item">
+                            <div class="grid place-items-center">
+                                <a href="#" class=" col-span-full row-span-full text-[#69707D] hover:text-black peer">{{
+                                    item }}</a>
+                                <hr
+                                    class="w-full col-span-full row-span-full peer-hover:border-b-2 peer-hover:border-b-[#FF7E1B] border-none peer-hover:border-solid translate-y-[calc(var(--desktop-header-height)/2+var(--desktop-header-padding-bottom))] ">
+                            </div>
+
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="ml-auto flex gap-8 items-center">
+                <ShoppingCart />
+                <Avatar />
+            </div>
+
         </div>
     </header>
 </template>
